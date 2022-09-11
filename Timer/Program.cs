655cb626349a74395f86a3b9f43c2670ee637dfd
Алексей Демьяненко <krossovka@outlook.com>
2022-09-11 15:08:46 +0300

    1

--- Timer/Program.cs
+++ Timer/Program.cs
@@ -1,40 +1,18 @@
-﻿internal class Program
+﻿class Program
 {
-    private static void Print(object obj)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-
     static void Main(string[] args)
     {
-        int num = 0;
+        
         TimerCallback timer = new TimerCallback(Print);
-        Timer timer1 = new Timer(timer, num, 0, 2000);
+        Timer timer1 = new Timer(timer, null, 0, 1000);
 
         Console.ReadLine();
     }
+
+    public static void Print(object obj)
+    {
+            Console.WriteLine("Timer is live");
+        
+        
+    }
 }
-
-
-
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        int num = 0;
-//        // устанавливаем метод обратного вызова
-//        TimerCallback tm = new TimerCallback(Count);
-//        // создаем таймер
-//        Timer timer = new Timer(tm, num, 0, 2000);
-
-//        Console.ReadLine();
-//    }
-//    public static void Count(object obj)
-//    {
-//        int x = (int)obj;
-//        for (int i = 1; i < 9; i++, x++)
-//        {
-//            Console.WriteLine($"{x * i}");
-//        }
-//    }
-//}
